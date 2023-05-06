7cb97e926dab611b0d7ba31cc225cfd191b9287c
mfried95 <m.friedman1995@gmail.com>
2023-05-06 13:48:33 -0400

    Added json data and assests in the public folder

---
+++ db/AppData.js
@@ -0,0 +1,64 @@
+export default [
+    
+    {
+        id: 'Appitisers',
+        name: "Yam Tempura",
+        description: " ",
+        image: "/../public/assets/apps/potato.jpeg",
+    },
+    {
+        id: 'app',
+        name: "Cheese Wontons",
+        dscription: "",
+        image: "/../public/assets/apps/cheese-wontons.jpeg",
+    },
+    {
+        id: 'app',
+        name: "Edamame",
+        dscription: "",
+        image: "/../public/assets/apps/edamame.jpeg",
+    },
+    {
+        id: 'app',
+        name: "Beef Skweres",
+        dscription: "",
+        image: "/../public/assets/apps/beef-skewers.jpg",
+    },
+    {
+        id: 'app',
+        name: "Chicken Fried Rice",
+        dscription: "",
+        image: "/../public/assets/apps/chicken-fried-rice.jpg",
+    },
+    {
+        id: 'app',
+        name: "Chicken katsu",
+        dscription: "",
+        image: "/../public/assets/apps/chicken-katsu.jpg",
+    },
+    {
+        id: 'app',
+        name: "Chicken Teriyaki ",
+        dscription: "",
+        image: "/../public/assets/apps/chicken-teriyaki.jpg",
+    },
+    {
+        id: 'app',
+        name: "Crab Meat Tempura",
+        dscription: "",
+        image: "/../public/assets/apps/crab-meat-tempura.jpg",
+    },
+    {
+        id: 'app',
+        name: "Eggplant",
+        dscription: "",
+        image: "/../public/assets/apps/Eggplant.jpg",
+    },
+    {
+        id: 'app',
+        name: "Coconut Shrimp",
+        dscription: "",
+        image: "/../public/assets/apps/coco-shrimp.jpg",
+    },
+
+]
