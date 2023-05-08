bb4d0653e21772ac8b865ce0b5987eaee6402853
yusufurur <yusufurur@gmail.com>
2023-05-08 13:30:30 -0400

    Added homepage and menu route

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const homepagesRoutes = require('./routes/homepages');
+const menuRoutes = require('./routes/menu');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +40,8 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/homepages', homepagesRoutes);
+app.use('/menu', menuRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
