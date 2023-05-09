a38a4ea07a310eac1b14776655393cd962b6bb73
Rida Batool <ridabatool.be13@gmail.com>
2023-05-09 16:32:08 -0400

    Create login functions

--- db/queries/dbQueries.js
+++ db/queries/dbQueries.js
@@ -1,4 +1,28 @@
 const db = require('../connection');
+const bcrypt = require("bcryptjs");
+
+//Get user info with email
+const getUserWithEmail = email => {
+  return db
+    .query(`
+  SELECT * FROM users WHERE email = $1;`, [email]
+    )
+    .then(res => res.rows[0]);
+};
+
+
+//login
+const login = function(email, password) {
+  return getUserWithEmail(email)
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    })
+    .catch(err => console.log(err));
+};
+
 
 const getMenuItems = function() {
   return db.query('SELECT * from menu_items;')
@@ -8,4 +32,4 @@ const getMenuItems = function() {
     });
 };
 
-module.exports = { getMenuItems };
+module.exports = { getMenuItems, getUserWithEmail, login };
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -6,11 +6,35 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const database = require("../db/queries/dbQueries");
 
+//login page-setup
 router.get('/', (req, res) => {
   console.log("Login Page");
-  res.render('login');
+  if (req.session.user_id) {
+    console.log(req.session.user_id);
+    res.redirect("/");
+  } else {
+    let templateVars = {
+      user_id: null
+    };
+    res.render('login', templateVars);
+  }
+});
+
+router.post("/", (req, res) => {
+  database
+    .login(req.body.email, req.body.password)
+    .then(user => {
+      if (!user) {
+        res.send("User account doesn't exist. Please register first. Thank you. :)");
+        return;
+      }
+      req.session.user_id = user.id;
+      res.redirect("/");
+    })
+    .catch(err => res.send(err));
 });
 
 module.exports = router;
