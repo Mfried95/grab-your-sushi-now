9602519747960b87295ca5b22688fc1854c2709f
yusufurur <yusufurur@gmail.com>
2023-05-09 14:31:59 -0400

    working on order route

--- db/queries/dbQueries.js
+++ db/queries/dbQueries.js
@@ -1,6 +1,6 @@
 const db = require('../connection');
 
-const getMenuItems = function() {
+const getMenuItems = function () {
   return db.query('SELECT * from menu_items;')
     .then(data => {
       console.log(data.rows);
@@ -8,4 +8,22 @@ const getMenuItems = function() {
     });
 };
 
-module.exports = { getMenuItems };
+const getUserInfo = function () {
+  return db.query('SELECT orders.id, users.id, users.name, users.email, users.address, users.phone_number, users.credit_card FROM orders JOIN cart_items on orders.id = cart_items.order_id JOIN users ON orders.user_id = users.id;')
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    });
+};
+
+const getOrderDetails = function (order_id) {
+  return db.query('SELECT * FROM cart_items WHERE order_id = $1', [order_id])
+    .then(data => {
+      console.log(data.rows);
+      return data.rows;
+    });
+};
+
+
+module.exports = { getMenuItems, getUserInfo, getOrderDetails};
+
