1c7577ef8d20958f05c8cd9ea70f2fe81ed4306e
mfried95 <m.friedman1995@gmail.com>
2023-05-10 15:47:01 -0400

    bycriptjs

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -7,7 +7,7 @@
 
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const database = require("../db/queries/dbQueries");
 const cookieSession = require("cookie-session");
 
