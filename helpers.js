968708f45512236ad2cc114585fdd190ebccc879
yusufurur <yusufurur@gmail.com>
2023-06-26 19:04:57 -0400

    updatd .env

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const sendSms = (phone, message) => {
-  console.log("sendSms");
+  console.log("sendSms!");
   const client = require("twilio")(accountSid, authToken);
   client.messages
     .create({
